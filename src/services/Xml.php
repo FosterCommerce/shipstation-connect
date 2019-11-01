ca2c8b5abdabab1b13a334e3c16e41f6df5d8068
jmauzyk <jmauzyk@gmail.com>
2019-11-01 10:50:43 -0500

    Replaced calls to deprecated getAdjustmentsTotalByType() method

--- src/services/Xml.php
+++ src/services/Xml.php
@@ -90,13 +90,13 @@ class Xml extends Component
             ],
             'TaxAmount' => [
                 'callback' => function ($order) {
-                    return $order->getAdjustmentsTotalByType('tax');
+                    return $order->getTotalTax();
                 },
                 'cdata' => false,
             ],
             'ShippingAmount' => [
                 'callback' => function ($order) {
-                    return $order->getAdjustmentsTotalByType('shipping');
+                    return $order->getTotalShippingCost();
                 },
                 'cdata' => false,
             ]
@@ -243,7 +243,7 @@ class Xml extends Component
     public function discount(\SimpleXMLElement $xml, Order $order, $name='Item')
     {
         // If no discount was applied, skip this
-        if ($order->getAdjustmentsTotalByType('discount') >= 0) {
+        if ($order->getTotalDiscount() >= 0) {
             return;
         }
 
@@ -265,7 +265,7 @@ class Xml extends Component
             ],
             'UnitPrice'  => [
                 'callback' => function ($order) {
-                    return number_format($order->getAdjustmentsTotalByType('discount'), 2);
+                    return number_format($order->getTotalDiscount(), 2);
                 },
                 'cdata' => false,
             ],
