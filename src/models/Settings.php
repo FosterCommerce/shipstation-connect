3ba9bfdf4c0258904e219325370e3f54849d4de4
Pete Eveleigh <pete@fostercommerce.com>
2023-09-27 12:29:58 +0100

    Add phone number field to settings

--- src/models/Settings.php
+++ src/models/Settings.php
@@ -18,4 +18,5 @@ class Settings extends Model
     public $serviceFieldHandle = 'service';
     public $trackingNumberFieldHandle = 'trackingNumber';
     public $billingSameAsShipping = false;
+    public $phoneNumberFieldHandle = '';
 }
